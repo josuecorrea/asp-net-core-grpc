e229cf6a2fb6ff293b26f0397c504ea16d90011b
Josué Corrêa <josuejtc@gmail.com>
2023-10-02 14:12:52 -0300

    test

---
+++ GrpcCall.Api/Program.cs
@@ -0,0 +1,24 @@
+using GrpcCall.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+
+app.MapGrpcService<GreeterService>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ GrpcCall.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Net.Client;
+using GrpcCall.Api.Protos;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrpcCall.Console
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:7172");
+            var client = new Greeter.GreeterClient(channel);
+
+            for (int i = 0; i < 5; i++)
+            {
+                var response = await client.SayHelloAsync(
+                    new HelloRequest { Name = "World" });
+
+                System.Console.WriteLine(response.Message);
+            }
+
+            while (true)
+            {
+                var response = await client.SayHelloAsync(
+                   new HelloRequest { Name = "World" });
+
+                System.Console.WriteLine(response.Message);
+
+                Thread.Sleep(2000);
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
